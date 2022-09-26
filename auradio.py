f8461650da9e6ced6fe4609daf93d673a456e33a
mertemr <mertemir38@hotmail.com>
2022-09-26 18:59:18 +0300

    code fully updated to OOP, some bugfixes and changes

--- auradio.py
+++ auradio.py
@@ -8,136 +8,152 @@
 # please increase this counter as a
 # warning for the next person:
 #
-# total_hours_wasted_here: 254
+# total_hours_wasted_here: 254.32
 
-from time import sleep
+if __import__("sys").platform == "win32":
+    print("Sorry, Auradio is not working on windows 😒")
+    exit(0)
+
 import curses
-import weakref
-import vlc
 import json
-import os
 
-#os.environ["VLC_VERBOSE"] = str("-1")
+from pathlib import Path
+from time import sleep
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.curs_set(0)
-stdscr.keypad(1)
-curses.mousemask(1)
+import vlc
 
-ses = 40
+# os.environ["VLC_VERBOSE"] = str("-1")
+class Auradio(object):
+    def __init__(self) -> None:
+        self.stdscr = curses.initscr()
+        self.stdscr.keypad(1)
+        self.volume = 40    
+        
+        curses.noecho()
+        curses.curs_set(0)
+        curses.mousemask(1)
+        
+        with open(Path("data.json"), 'r') as f:
+            self.radio: list[dict] = json.load(f)
+        
+        self.switcher = {i:100 - (i-1) * 10 for i in range(1, 12)}
+        self.title()
 
-loc = os.path.dirname(__file__)
-with open(loc + '/data.json') as f:
-    radio = json.load(f)
+    def title(self):
+        self.addstr(
+        """                              
+                            _ _     
+  __ _ _   _ _ __ __ _  __| (_) ___   
+ / _` | | | | '__/ _` |/ _` | |/ _ \  
+| (_| | |_| | | | (_| | (_| | | (_) | 
+ \__,_|\__,_|_|  \__,_|\__,_|_|\___/  
 
-
-def auradio():
-    stdscr.addstr('''                              
-                                _ _        
-       __ _ _   _ _ __ __ _  __| (_) ___   
-      / _` | | | | '__/ _` |/ _` | |/ _ \  
-     | (_| | |_| | | | (_| | (_| | | (_) | 
-      \__,_|\__,_|_|  \__,_|\__,_|_|\___/  
-                                           
-    ''')
-    stdscr.refresh()
-    sleep(0.4)
-    stdscr.clear()
-
-
-def playerset(url):
-    global ses
-
-    vlc_instance = vlc.Instance('-q')
-    player = vlc_instance.media_player_new()
-
-    player.set_media(vlc_instance.media_new(url))
-    player.audio_set_volume(ses)
-
-    player.play()
-    return player
+    """)
+        self.stdscr.refresh()
+        sleep(1)
+        self.stdscr.clear()
+        
 
 
-def volbar():
-    stdscr.addstr(0, 0, '┌')
-    stdscr.addstr(0, 2, '┐')
-    stdscr.addstr(11, 0, '└')
-    stdscr.addstr(11, 2, '┘')
+    def playerset(self, url: str):
+        vlc_instance = vlc.Instance("-q")
+        self.player = vlc_instance.media_player_new()
 
-    for i in range(1, 11):
-        stdscr.addstr(i, 0, '│')
-        stdscr.addstr(i, 2, '│')
-        stdscr.addstr(i, 1, '─')
+        self.player.set_media(vlc_instance.media_new(url))
+        self.player.audio_set_volume(self.volume)
 
-    a = int(ses/10)
-    while a > 0:
-        stdscr.addstr(11 - a, 1, '▆')
-        a = a - 1
+        self.player.play()
 
+    def addstr(self, *args):
+        self.stdscr.addstr(*args)
 
-def setvol(argument):
-    switcher = {1: 100, 2: 90, 3: 80, 4: 70, 5: 60,
-                6: 50, 7: 40, 8: 30, 9: 20, 10: 10, 11: 0, }
-    return switcher.get(argument, "nothing")
+    def volbar(self):
+        self.addstr(0, 0, "┌")
+        self.addstr(0, 2, "┐")
+        self.addstr(11, 0, "└")
+        self.addstr(11, 2, "┘")
 
+        for i in range(1, 11):
+            self.addstr(i, 0, "│")
+            self.addstr(i, 2, "│")
+            self.addstr(i, 1, "─")
 
-def radioset():
-    now = -1
+        a = int(self.volume / 10)
+        while a > 0:
+            self.addstr(11 - a, 1, "▆")
+            a = a - 1
 
-    while True:
-        radx = 4
-        rady = 1
+    def setvol(self, num: int):
+        return self.switcher.get(num)
 
-        for w in radio:
-            stdscr.addstr(rady, radx, str(w["name"]))
+    def setradio(self):
+        now = -1
 
-            if (int(w["number"]) == now):
-                stdscr.addstr(rady, radx, str(w["name"]), curses.A_BOLD)
+        while True:
+            radx = 4
+            rady = 1
 
-            if rady == 10:
-                rady = 0
-                radx = radx + 23
-                for i in range(1, 11):
-                    stdscr.addstr(i, radx-2, '│')
-            rady = rady + 1
+            for w in self.radio:
+                self.addstr(rady, radx, str(w["name"]))
 
-        volbar()
+                if int(w["number"]) == now:
+                    self.addstr(rady, radx, str(w["name"]), curses.A_BOLD)
 
-        event = stdscr.getch()
+                if rady == 10:
+                    rady = 0
+                    radx = radx + 23
+                    for i in range(1, 11):
+                        self.addstr(i, radx - 2, "│")
+                rady = rady + 1
 
-        if event == curses.KEY_MOUSE:
-            try: click = curses.getmouse()
-            except: break
+            self.volbar()
 
-            if (click[2] <= 10 and click[2] >= 1 and click[1] < (int((len(radio)/10)*23)+2)):
+            event = self.stdscr.getch()
 
-                if click[1] < 3:
-                    global ses
-                    ses = setvol(click[2])
-                    try:
-                        scream.audio_set_volume(ses)
-                        continue
-                    except: pass
+            if event == curses.KEY_MOUSE:
+                try:
+                    click = curses.getmouse()
+                except Exception:
+                    break
+                except KeyboardInterrupt:
+                    self.close()
 
-                # i dont know whats going on and
-                # at this point i'm too afraid to ask
-                now = (int((click[1]-2)/23)*10+click[2]-1)
+                if (
+                    click[2] <= 10
+                    and click[2] >= 1
+                    and click[1] < (int((len(self.radio) / 10) * 23) + 2)
+                ):
 
-                for w in radio:
-                    if (str(w["number"]) == str(now)):
-                        
-                        try: scream.stop()
-                        except: pass
+                    if click[1] < 3:
+                        self.volume = self.setvol(click[2])
+                        try:
+                            self.player.audio_set_volume(self.volume)
+                            continue
+                        except Exception:
+                            pass
 
-                        scream = playerset(str(w["url"]))
+                    # i dont know whats going on and
+                    # at this point i'm too afraid to ask
+                    now = int((click[1] - 2) / 23) * 10 + click[2] - 1
 
-        stdscr.refresh()
+                    for w in self.radio:
+                        if str(w["number"]) == str(now):
+                            try:
+                                self.player.stop()
+                            except:
+                                pass
+                            self.playerset(str(w["url"]))
+            self.stdscr.refresh()
+
+    def close(self):
+        self.player.close()
+        raise SystemExit()
 
 if __name__ == "__main__":
-    stdscr.clear()
-    stdscr.refresh()
-    auradio()
-    radioset()
-
-curses.endwin()
+    auradio = Auradio()
+    try:
+        auradio.setradio()
+    except Exception as e:
+        print(e)
+    finally:
+        curses.endwin()
